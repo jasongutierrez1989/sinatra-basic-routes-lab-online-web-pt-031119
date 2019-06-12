cdcb9d938a17a80f78eb283ca680b40602169225
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-12 01:40:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,16 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-  get '/' do
+  get '/name' do
     "My name is __"
   end
 
-  get '/' do
+  get '/hometown' do
     "My hometown is __"
   end
 
-  get '/' do
+  get '/favorite-song' do
     "My favorite song is __"
   end
 end
